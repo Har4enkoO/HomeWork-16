aee0ebc45713e7418778cbde113108c0e7a19680
har4enko <pan4enko48@gmail.com>
2020-04-06 14:46:23 +0300

    Updates

---
+++ precache-manifest.de41b90e14b5806357c387e3781f95a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "653e65e79b15c53e698e7069ffd26d86",
+    "url": "/HomeWork-16/index.html"
+  },
+  {
+    "revision": "20c9a71ecc1785dac27e",
+    "url": "/HomeWork-16/static/css/main.49a64464.chunk.css"
+  },
+  {
+    "revision": "3caa83283409033d2816",
+    "url": "/HomeWork-16/static/js/2.017cc6e2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/HomeWork-16/static/js/2.017cc6e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20c9a71ecc1785dac27e",
+    "url": "/HomeWork-16/static/js/main.d0a2cfc4.chunk.js"
+  },
+  {
+    "revision": "d095a854ad749afddcc2",
+    "url": "/HomeWork-16/static/js/runtime-main.e58a159e.js"
+  }
+]);
